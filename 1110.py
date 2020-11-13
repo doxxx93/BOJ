becd163be8d150562a1c6985bfad08eef1bed2c3
Doyul <doxxx93@gmail.com>
2020-11-13 21:44:35 +0900

    BOJ1110

---
+++ 1110.py
@@ -0,0 +1,14 @@
+num =n = 1
+count =0
+while True:
+    a= int(num//10)
+    
+    b = int(num%10)
+    
+    num = a+b
+    count +=1  
+    num = int(f"{b}{num%10}")
+    
+    if num == n:
+        break
+print(count)
