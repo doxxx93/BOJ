47202baf1fa0375b02bb9df6f1723a9e02684bb5
Doyul <doxxx93@gmail.com>
2020-11-22 21:12:04 +0900

    BOJ T_T

--- 15649.py
+++ 15649.py
@@ -1,8 +1,8 @@
 n = 4
 m = 4
 
-for i in range(1,n+1):
-    
-    for j in range(1,m+1):
-        print(j,end =" ")
+for i in range(1, n+1):
+
+    for j in range(1, m+1):
+        print(j, end=" ")
     print("")
